8d4cfd2188f3303f73a3838df64d812eb439b692
breab <brea.borlas@gmail.com>
2018-06-05 13:53:49 -0700

    Add login component and routing

--- co-work-sass/src/app/app.module.ts
+++ co-work-sass/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
