4362e9aa34bf815f746f07d603af9313f2125e9a
breab <brea.borlas@gmail.com>
2018-06-05 13:46:45 -0700

    Re-set up routing

--- co-work-sass/src/app/app.module.ts
+++ co-work-sass/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+ import { routing } from './app.routing';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
